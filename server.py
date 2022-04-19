8a0f4a33395857cbaf40b44ad1e7028ff828c7d6
Yuantai Xu <yuantaixu@YuantaideMBP.lan>
2022-04-18 21:44:32 -0400

    Combine js and html

--- server.py
+++ server.py
@@ -4,68 +4,58 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
-data_for_quiz1 = ['https://www.koty.pl/wp-content/uploads/2019/08/kocie-uszka-2-864x575.jpg',
-                  'https://resc-files-prod.s3.us-west-1.amazonaws.com/s3fs-public/styles/large/public/2018-12/SLC-Feral-ear-tip-9285_1.jpg?VersionId=5Se7m9aAtpsvPIoiCAklTu1Yw.y44_Zq&itok=1Q4mwc2h',
-                  'https://culturafelina.com/wp-content/uploads/2018/02/angry_cat.jpg']
-data_for_quiz2 = ['https://pictures-of-cats.org/wp-content/uploads/2020/06/American-ringtail-cat-2A.jpg',
-                  'https://i.pinimg.com/564x/1a/c1/e5/1ac1e57b52f9764a247effd7c0df5821.jpg',
-                  'https://radiomitre-la100-prod.cdn.arcpublishing.com/resizer/hZpLaZuzpW00ajz5_OTTmJGpZ_A=/1200x0/smart/cloudfront-us-east-1.images.arcpublishing.com/radiomitre/O72VFR474JFJZPR4CH27RNBUXU.jpg',
-                  'https://catinfodetective.com/wp-content/uploads/2020/12/cat-4410834_1280-1-300x200.jpg',
-                  'https://srbigotesdegato.com/wp-content/uploads/2019/05/bosques-de-siberia-raza-gato.jpg']
-data_for_quiz3 = ['https://qph.cf2.quoracdn.net/main-qimg-63eb8dd985d48fe45280355f2c2ab6cc-pjlq',
-                  'https://static.onecms.io/wp-content/uploads/sites/34/2018/07/12170444/cat-rolling-over-on-back-getty-87883698.jpg',
-                  'https://bloximages.newyork1.vip.townnews.com/wfsb.com/content/tncms/assets/v3/editorial/8/fb/8fb3f3bd-0c9d-5f55-be93-8960a8967993/5e5958c592f48.image.jpg',
-                  'https://cdn.shopify.com/s/files/1/0344/6469/files/image_14_large.jpg?v=1484682446']
-data_for_quiz4 = ['https://www.tuxedo-cat.co.uk/wp-content/uploads/2021/07/cat-lying-on-back-with-paws-up-in-sun.jpg?ezimgfmt=rs:382x218/rscb1/ng:webp/ngcb1']
-choices_for_quiz4 = ["A. It's simply communicating a level of trust and confidence in your friendship.",
-                     "B. If this pose is actually followed with a hissing sound or a growl, you can touch them.",
-                     "C. They are mostly communicating happiness and satisfaction."]
-audio_for_quiz5 = {"Low or drawn out": "https://4170audio.s3.amazonaws.com/low.wav",
-                   "Soft pleading": "https://4170audio.s3.amazonaws.com/soft.wav",
-                   "Trilling": "https://4170audio.s3.amazonaws.com/trilling.wav",
-                   "Yowling": "https://4170audio.s3.amazonaws.com/yowling.wav",
-                   "Chirping": "https://4170audio.s3.amazonaws.com/cat-bird-chirp.mp3"}
-img_for_quiz5 = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2irskMPkz6T10bt_eadBkqQNduJnYHLRLFQ&usqp=CAU"]
-choices_for_quiz5 = ["A. Lure their unassuming prey.",
-                     "B. Dissatisfaction or a lack of interest.",
-                     "C. It is hungry and will like to be fed immediately.",
-                     "D. To get your attention and sometimes with cute eyes",
-                     "E. A call for mating."]
-choices_for_quiz_review = ["A. If your cat want to knead on your lap, it is actual a good sign of contentment and happiness.",
-                     "B. Most time when your cat wiggles its butt, it's getting ready to escape.",
-                     "C. The cat will let out a series of short, high pitched meows to tell you it's glad you're back.",
-                     "D. Cats are hunters. Sometimes they make chirping sounds like criskets to lure their unassuming prey."]
-data_for_quiz_end = ['https://static-cdn.jtvnw.net/jtv_user_pictures/b6be9ae8-3433-4a32-bc20-a749781ca0f5-profile_banner-480-320x160.jpeg']
+data = {
+    "1": {"question": "Q1: Which one does the cat's ears slight bent forward? (10 pt)",
+          "images": ['https://www.koty.pl/wp-content/uploads/2019/08/kocie-uszka-2-864x575.jpg',
+                     'https://resc-files-prod.s3.us-west-1.amazonaws.com/s3fs-public/styles/large/public/2018-12/SLC-Feral-ear-tip-9285_1.jpg?VersionId=5Se7m9aAtpsvPIoiCAklTu1Yw.y44_Zq&itok=1Q4mwc2h',
+                     'https://culturafelina.com/wp-content/uploads/2018/02/angry_cat.jpg']},
+    "2": {"question": "Q2: Which one is the puffed up tail? (10 pt)",
+          "images": ['https://pictures-of-cats.org/wp-content/uploads/2020/06/American-ringtail-cat-2A.jpg',
+                     'https://i.pinimg.com/564x/1a/c1/e5/1ac1e57b52f9764a247effd7c0df5821.jpg',
+                     'https://radiomitre-la100-prod.cdn.arcpublishing.com/resizer/hZpLaZuzpW00ajz5_OTTmJGpZ_A=/1200x0/smart/cloudfront-us-east-1.images.arcpublishing.com/radiomitre/O72VFR474JFJZPR4CH27RNBUXU.jpg',
+                     'https://catinfodetective.com/wp-content/uploads/2020/12/cat-4410834_1280-1-300x200.jpg',
+                     'https://srbigotesdegato.com/wp-content/uploads/2019/05/bosques-de-siberia-raza-gato.jpg']},
+    "3": {"question": "Q3: Select the picture(s) that the cat in a positive mood? (10 pt)",
+          "images": ['https://qph.cf2.quoracdn.net/main-qimg-63eb8dd985d48fe45280355f2c2ab6cc-pjlq',
+                     'https://static.onecms.io/wp-content/uploads/sites/34/2018/07/12170444/cat-rolling-over-on-back-getty-87883698.jpg',
+                     'https://bloximages.newyork1.vip.townnews.com/wfsb.com/content/tncms/assets/v3/editorial/8/fb/8fb3f3bd-0c9d-5f55-be93-8960a8967993/5e5958c592f48.image.jpg',
+                     'https://cdn.shopify.com/s/files/1/0344/6469/files/image_14_large.jpg?v=1484682446']},
+    "4": {"question": "Q4: What's the best description of the left picture? (10 pt)",
+          "images": [
+              'https://www.tuxedo-cat.co.uk/wp-content/uploads/2021/07/cat-lying-on-back-with-paws-up-in-sun.jpg?ezimgfmt=rs:382x218/rscb1/ng:webp/ngcb1'],
+          "choices": ["A. It's simply communicating a level of trust and confidence in your friendship.",
+                      "B. If this pose is actually followed with a hissing sound or a growl, you can touch them.",
+                      "C. They are mostly communicating happiness and satisfaction."]},
+    "5": {"question": "Q5: Match the meaning of different cat meows. (50 pt)",
+          "images": [
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2irskMPkz6T10bt_eadBkqQNduJnYHLRLFQ&usqp=CAU"],
+          "choices": ["A. Lure their unassuming prey.",
+                      "B. Dissatisfaction or a lack of interest.",
+                      "C. It is hungry and will like to be fed immediately.",
+                      "D. To get your attention and sometimes with cute eyes",
+                      "E. A call for mating."],
+          "audios": {"Low or drawn out": "https://4170audio.s3.amazonaws.com/low.wav",
+                     "Soft pleading": "https://4170audio.s3.amazonaws.com/soft.wav",
+                     "Trilling": "https://4170audio.s3.amazonaws.com/trilling.wav",
+                     "Yowling": "https://4170audio.s3.amazonaws.com/yowling.wav",
+                     "Chirping": "https://4170audio.s3.amazonaws.com/cat-bird-chirp.mp3"}},
+    "6": {"question": "Review: Choose the correct one(s) from the following statements (10 pt)",
+          "choices": [
+              "A. If your cat want to knead on your lap, it is actual a good sign of contentment and happiness.",
+              "B. Most time when your cat wiggles its butt, it's getting ready to escape.",
+              "C. The cat will let out a series of short, high pitched meows to tell you it's glad you're back.",
+              "D. Cats are hunters. Sometimes they make chirping sounds like criskets to lure their unassuming prey."]},
+    "7": {"question": "You've already known enough about kittens, hope you will have your own cat.",
+          "images": [
+              'https://static-cdn.jtvnw.net/jtv_user_pictures/b6be9ae8-3433-4a32-bc20-a749781ca0f5-profile_banner-480-320x160.jpeg']}
+}
+
 
 # ROUTES
+@app.route('/quiz/<id>')
+def quiz(id=None):
+    return render_template("quiz.html", data=data[id], id=id)
 
-@app.route('/quiz/quiz_1')
-def quiz_1():
-   return render_template('quiz_1.html', data=data_for_quiz1)
-
-@app.route('/quiz/quiz_2')
-def quiz_2():
-   return render_template('quiz_2.html', data=data_for_quiz2)
-
-@app.route('/quiz/quiz_3')
-def quiz_3():
-   return render_template('quiz_3.html', data=data_for_quiz3)
-
-@app.route('/quiz/quiz_4')
-def quiz_4():
-   return render_template('quiz_4.html', data=data_for_quiz4, choices=choices_for_quiz4)
-
-@app.route('/quiz/quiz_5')
-def quiz_5():
-   return render_template('quiz_5.html', data=audio_for_quiz5, choices=choices_for_quiz5, img=img_for_quiz5)
-
-@app.route('/quiz/quiz_review')
-def quiz_review():
-   return render_template('quiz_review.html', choices=choices_for_quiz_review)
-
-@app.route('/quiz/quiz_end')
-def quiz_end():
-   return render_template('quiz_end.html', data=data_for_quiz_end)
 
 if __name__ == '__main__':
    app.run(debug = True)
---
+++ static/quiz.js
@@ -0,0 +1,137 @@
+function addButton(id){
+    let next_id = id + 1
+    let prev_id = id - 1
+    let but_div = $("<div class='button_container'></div>")
+    let submit_button = $("<button type='button' class='btn btn-primary btn-lg next_button' " +
+        "onclick=\"location.href='" + next_id + "';\">Submit</button>")
+    let next_button = $("<button type='button' class='btn btn-primary btn-lg next_button' " +
+        "onclick=\"location.href='" + next_id + "';\">Next ></button>")
+    let prev_button = $("<button type='button' class='btn btn-primary btn-lg prev_button' " +
+        "onclick=\"location.href='" + prev_id + "';\">< Previous</button>")
+    if(prev_id != 0 && prev_id != 6){
+        $(but_div).append(prev_button)
+    }
+    if(next_id != 7 && next_id != 8){
+        $(but_div).append(next_button)
+    }
+    if(next_id == 7){
+        $(but_div).append(submit_button)
+    }
+    $("#quiz_container").append(but_div)
+}
+
+function addQuestion(data, id){
+    if(id == 7){
+        let question_div = $("<div id='quiz_7_body'>" + data + "</div>")
+        $("#quiz_container").append(question_div)
+    }
+    else{
+        console.log(data)
+        let question_title = data.substr(0,4)
+        let question_body = data.substr(4)
+        let question_div = $("<div class='question_body'><span class='question_title'>" +
+            question_title + "</span>" + question_body + "</div>")
+        $("#quiz_container").append(question_div)
+    }
+}
+
+function display123(data, id){
+    let body_div = $("<div id='quiz_" + id + "_container'></div>")
+    $.each(data, function(index, value){
+        let name = "Q" + id + index
+        let new_img= $("<input type='checkbox' name='" + name + "' class='quiz" + id +"_img'" +
+            "id='" + name + "'/><label for='" + name + "'></label>")
+        $(new_img).css("background-image", "url('" + value + "')")
+        $(body_div).append(new_img)
+        $("#quiz_container").append(body_div)
+    })
+}
+
+function display4(img, choices){
+    let body_div = $("<div id='quiz_4_container'></div>")
+    let img_div = $("<div id='quiz_4_img_container'></div>")
+    let choice_div = $("<div id='quiz_4_choices_container'></div>")
+    let new_img= $("<img class='quiz4_img' src='" + img[0] + "'>")
+    $(img_div).append(new_img)
+    $.each(choices, function(index, value){
+        let name = "Q4" + index
+        let new_choice= $("<input type='checkbox' name='" + name + "' class='quiz4_choice'" +
+            "id='" + name + "'/><label for='" + name + "'>" + value + "</label><br>")
+        $(choice_div).append(new_choice)
+    })
+    $(body_div).append(img_div)
+    $(body_div).append(choice_div)
+    $("#quiz_container").append(body_div)
+}
+
+function display5(img, choices, audios){
+    let body_div = $("<div id='quiz_5_container'></div>")
+    let player_div = $("<div id='quiz_5_player_container'></div>")
+    let choice_div = $("<div id='quiz_5_choices_container'></div>")
+    let audio_div = $("<div id='quiz_5_audio_container'></div>")
+    let i = 0
+    $.each(audios, function(key, value){
+        let player_name = "Q5P" + i
+        let audio_name = "Q5A" + i
+        let type = "audio/" + value.substr(value.length - 3)
+        if(type == "audio/mp3"){
+            type = "audio/mpeg"
+        }
+        let new_player= $("<input type='text' name='" + player_name + "' class='quiz5_input'" +
+            "id='" + player_name + "' maxlength='1' size='2'/><label for='" + player_name + "'>" + key + "</label>" +
+            "<img src='" + img + "' class='quiz5_img' id='quiz5_img" + i + "'><br>")
+        $(player_div).append(new_player)
+        let new_audio = $("<audio id='" + audio_name + "'><source src='" + value +
+            "' type='" + type + "'></audio>")
+        $(audio_div).append(new_audio)
+        i += 1
+    })
+    $.each(choices, function(index, value){
+        let name = "Q5C" + index
+        let new_choice= $("<div class='quiz5_choice'" + "id='" + name + "'>" + value +"</div><br>")
+        $(choice_div).append(new_choice)
+    })
+    $(body_div).append(player_div)
+    $(body_div).append(choice_div)
+    $(body_div).append(audio_div)
+    $("#quiz_container").append(body_div)
+}
+
+function display6(choices){
+    let body_div = $("<div id='quiz_6_container'></div>")
+    $.each(choices, function(index, value){
+        let name = "Q6" + index
+        let new_choice= $("<input type='checkbox' name='" + name + "' class='quiz_6_choice'" +
+            "id='" + name + "'/><label for='" + name + "'>" + value + "</label><br>")
+        $(body_div).append(new_choice)
+    })
+    $("#quiz_container").append(body_div)
+}
+
+function display7(img){
+    let body_div = $("<div id='quiz_7_container'></div>")
+    let new_img= $("<img class='quiz7_img' src='" + img[0] + "'>")
+    $(body_div).append(new_img)
+    $("#quiz_container").append(body_div)
+}
+
+$(document).ready(function(){
+    console.log(typeof(id))
+    addButton(id)
+    addQuestion(data['question'], id)
+    if (id == 1 || id == 2 || id == 3) {
+        display123(data['images'], id)
+    } else if (id == 4) {
+        display4(data['images'], data['choices'])
+    } else if (id == 5) {
+        display5(data['images'], data['choices'], data['audios'])
+        $(document).on('click', '.quiz5_img', function (event) {
+            let id = $(this).attr("id").substr($(this).attr("id").length - 1)
+            $("#Q5A" + id)[0].play()
+        })
+    } else if (id == 6) {
+            display6(data['choices'])
+    } else{
+        display7(data['images'])
+    }
+})
